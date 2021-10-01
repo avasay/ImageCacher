16867d1b50beb26f6036cb65bc67ccd4139a04ed
alejandrio.vasay <alejandrio.vasay@tccd.edu>
2021-10-01 09:28:38 -0500

    First commit

---
+++ ImageCacher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ImageCacherConsoleApp;
+
+public class ImageCacher
+{
+    IMemoryCache m_memoryCache;
+    private ImageObject? m_imageObject;
+    private string m_pickedImagePath;
+
+    public ImageCacher(IMemoryCache memoryCache, string pickedImagePath)
+    {
+        m_memoryCache = memoryCache;
+        m_imageObject = null;
+
+        if (File.Exists(pickedImagePath))
+        {
+            m_pickedImagePath = pickedImagePath;
+        }
+        else
+        {
+            throw new FileNotFoundException();
+        };
+    }
+
+    public string GetImage()
+    {
+        string msg = string.Empty;
+        m_memoryCache.TryGetValue(m_pickedImagePath, out m_imageObject);
+
+        if (m_imageObject == null)
+        {
+            // Read time stamp of file.
+            DateTime ourFileDate = File.GetLastWriteTime(m_pickedImagePath);
+            ourFileDate = ourFileDate.AddMilliseconds(-ourFileDate.Millisecond);
+
+            // Open the file and read the contents into a byte array
+            byte[] byteArray = File.ReadAllBytes(m_pickedImagePath);
+            m_imageObject = new ImageObject(m_pickedImagePath, "image/jpeg", byteArray, ourFileDate);
+
+            // Set cache options
+            var memCacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(120));
+
+            // Cache the image
+            m_memoryCache.Set(m_pickedImagePath, m_imageObject, memCacheEntryOptions);
+
+            msg = "Loading image from drive.";
+        }
+        else
+        {
+            msg = "Loading image from cache.";
+        }
+
+        return msg;
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => services.AddMemoryCache())
+    .Build();
+
+IMemoryCache cache = host.Services.GetRequiredService<IMemoryCache>();
+
+string image1 = Environment.CurrentDirectory + @"\Images\abstract\" + "timur-kozmenko-GS84KG8yNDo-unsplash.jpg";
+string image2 = Environment.CurrentDirectory + @"\Images\abstract\" + "timur-kozmenko-VJj70LBdrnM-unsplash.jpg";
+string image3 = Environment.CurrentDirectory + @"\Images\abstract\" + "timur-kozmenko-zxVdi0iEO7M-unsplash.jpg";
+
+ImageCacherConsoleApp.ImageCacher image1Cacher = new ImageCacherConsoleApp.ImageCacher(cache, image1);
+ImageCacherConsoleApp.ImageCacher image2Cacher = new ImageCacherConsoleApp.ImageCacher(cache, image2);
+ImageCacherConsoleApp.ImageCacher image3Cacher = new ImageCacherConsoleApp.ImageCacher(cache, image3);
+
+// First Pass
+
+Console.WriteLine(image1Cacher.GetImage());
+
+Console.WriteLine(image2Cacher.GetImage());
+
+Console.WriteLine(image2Cacher.GetImage());
+
+// Second Pass
+
+Console.WriteLine(image1Cacher.GetImage());
+
+Console.WriteLine(image2Cacher.GetImage());
+
+Console.WriteLine(image3Cacher.GetImage());
